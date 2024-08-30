936ef965db3d1189455b4cce84a7461c6cff7f3a
solaimanb <solaimanb.dev@gmail.com>
2024-08-30 22:31:05 +0600

    initial layout setup

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from kivy.app import App
-from kivy.uix import Widget
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+
+class BoxLayoutCanvas(BoxLayout):
+    pass
+    # def __init__(self, **kwargs):
+    #     super().__init__(**kwargs)
+    #     self.orientation = "vertical"
+    #     b1 = Button(text="Button 1")
+    #     b2 = Button(text="Button 2")
+    #     self.add_widget(b1)
+    #     self.add_widget(b2)
 
 class RetroWidget(Widget):
     pass
 
-class RetroLab(App):
+class RetroLabApp(App):
     pass
 
-RetroLab().run()
+RetroLabApp().run()
